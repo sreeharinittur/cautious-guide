81a712fcb676051f4e08d9ba76d4b96e76290cb5
Shrihari <sreeharinittur2001@gmail.com>
2024-05-18 16:24:56 +0530

    This is my fastapi app just to learn devops

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root()->dict:
+    """
+    root endpoint
+     :return: returns as json object
+    """
+    return {"message": "Hello World"}
+
+@app.get('/mypage')
+def cus_index()->dict:
+    """
+    custom route
+     :return: returns as json object with a message key
+    """
+    return "This is a custom message"
