6833104663d915982d707afbd81995a77197e5c9
Shrihari <sreeharinittur2001@gmail.com>
2024-05-19 10:16:03 +0530

    added flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    """
+    returns hello world
+    """
+    return "<p>Hello, World!</p>"
+
+@app.route('/dashboard')
+def hello_dashboard():
+    """
+    navigates to dashboard
+    """
+    return "<h1>DASHBOARD</h2>"
